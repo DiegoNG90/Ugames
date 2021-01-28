ae93154edcfa90520df5157794686e4fccd68e68
Diego <diego.nicolas.gandara@gmail.com>
2021-01-28 19:16:35 -0300

    Muevo public dentro de API

--- API/server.js
+++ API/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const port = 8080;
+const path = require('path');
 
+app.use(express.static(__dirname+'/public/'));
 
-app.use(express.static(__dirname+'/public/'))
 app.get('/', (req,res) => {
-    res.
+    res.render("/index.html")
 })
+
+app.listen(port, ()=> console.log("Escuchando en puerto " + port));
