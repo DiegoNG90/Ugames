13c8f9f14bc7d24bc2c3fcc6a95e96df5715599c
Diego <diego.nicolas.gandara@gmail.com>
2021-01-30 18:37:49 -0300

    Agregamos hbs - handlebars - a nuestro proyecto

--- API/server.js
+++ API/server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const path = require('path');
+const hbs = require('hbs');
 
 app.use(express.static(__dirname+'/public/'));
 
 app.get('/', (req,res) => {
-    res.render("/index.html")
+    res.render("/")
 })
 
 app.listen(port, ()=> console.log("Escuchando en puerto " + port));
