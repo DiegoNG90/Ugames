fd864a6b943849052c348add6f92a20d2204c7b6
Diego <diego.nicolas.gandara@gmail.com>
2021-01-30 21:47:30 -0300

    Rama Handlebars
    Fase experimental: probar el renderizado de la web con el uso de hbs.

--- API/server.js
+++ API/server.js
@@ -5,9 +5,15 @@ const path = require('path');
 const hbs = require('hbs');
 
 app.use(express.static(__dirname+'/public/'));
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname+"/views/partials")
+
 
 app.get('/', (req,res) => {
-    res.render("/")
+    res.render("home",{
+        anio: new Date().getFullYear(),
+        titulo: "Ugames"
+    })
 })
 
 app.listen(port, ()=> console.log("Escuchando en puerto " + port));
