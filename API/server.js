d28e524c386a53b04817f5be101b76e9a03e4ab6
Diego <diego.nicolas.gandara@gmail.com>
2021-02-04 19:17:11 -0300

    Instalo express-mysql-sessions y configuro el objeto session)

--- API/server.js
+++ API/server.js
@@ -3,6 +3,13 @@ const app = express();
 const port = 8080;
 const path = require('path');
 const hbs = require('hbs');
+const session = require('express-session');
+
+app.use(session({
+    secret: "triplete de messi al real",
+    resave: true,
+    saveUninitialized: true
+}))
 
 app.use(express.static(__dirname+'/public/'));
 
