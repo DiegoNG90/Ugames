b2154b80a7f9e1e2f04fecbd4d31d5b3cd8266bd
Diego <diego.nicolas.gandara@gmail.com>
2021-02-05 14:00:39 -0300

    Generamos las configuraciones necesarias entre la base de datos, la sessionStore y le pasamos a nuestra cookie la store 'sessionStore'

--- API/server.js
+++ API/server.js
@@ -6,11 +6,30 @@ const hbs = require('hbs');
 const session = require('express-session');
 const mysql = require('mysql');
 const MySQLStore = require('express-mysql-session')(session);
+//Configuracion de la BDD bd_games
+const options = {
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password:"",
+    database: "bd_ugames"
+}
+//generamos la conexion a msql con la configuracion de options
+const dbConnection = mysql.createConnection(options);
+//Ejecutamos la conexion a la BDD bd_games
+dbConnection.connect((err)=>{
+    if(err) throw err;
+    console.log("Conexion exitosa a la BDD");
+})
 
+//Creamos la sqlstore 
+const sessionStore = new MySQLStore(options);
+//Cookie
 app.use(session({
     secret: "triplete de messi al real",
     resave: true,
-    saveUninitialized: true
+    saveUninitialized: true,
+    store: sessionStore
 }))
 
 app.use(express.static(__dirname+'/public/'));
