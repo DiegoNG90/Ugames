8bf59858571bd50bb24b3b9933d71a9b7a540332
Diego <diego.nicolas.gandara@gmail.com>
2021-02-08 20:31:49 -0300

    genero los eventos del front para login y registro
    Genero los event listeners on click para manejar las rutas post hacia el back y así poder enviarle la data al back.

---
+++ API/public/js/loginYRegistro.js
@@ -0,0 +1,39 @@
+function registrarse(e){
+    e.preventDefault();
+    let username = document.querySelector('#userReg').value;
+    let password = document.querySelector('#passwordReg').value;
+    axios.post('http://localhost:8080/registro',{
+        username: username,
+        password: password,
+    })
+    .then((response)=>{
+        console.log(response);
+        window.location = 'landing.html';
+    })
+    .catch((err)=> {
+        throw (err)
+    })
+}
+document.querySelector('#reg-btn').addEventListener('click',registrarse);
+
+function logIn(e){
+    e.preventDefault();
+    let username = document.querySelector('#user').value;
+    let password = document.querySelector('#contraseña').value;
+    axios.post('http://localhost:8080/login',{
+        username: username,
+        password: password
+    })
+    .then((response)=>{
+        console.log(response.)
+        if(response.status===200){
+            console.log("Respuesta 200, el back te redirigió a landing.html")
+            // window.location = 'landing.html';
+        }
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+    // window.location = 'contenido.html'
+}
+document.querySelector('#login-btn').addEventListener('click',logIn);
