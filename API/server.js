b0f89a1ef75cedc276b807b06686019e8af72850
Diego <diego.nicolas.gandara@gmail.com>
2021-02-09 17:20:11 -0300

    Agrego todos los módulos referentes a sessions y passport. También hay algunos que quizás no se usan, como la const mysql -que ya está en bdConfig.js- y el que es const {urlencoded}. Queda pendiente revisar si se usan o no y en todo caso borrar esas constantes. También path y hbs quedaron medio en el aire :')

--- API/server.js
+++ API/server.js
@@ -2,10 +2,19 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const path = require('path');
-const hbs = require('hbs');
+const hbs = require('hbs'); // No se usa; ver branch hbs
 const cors = require('cors');
 const juegosRouter = require('./routes/games');
 const imgRoutes = require('./routes/img');
+const {options, conexion_bd} = require('./config/bdConfig')
+//Passport y sessions
+const session = require('express-session'); 
+const mysql = require('mysql'); // CHEQUEAR SI ES NECESARIO QUE ESTÉ ACÁ!
+const MySQLStore = require('express-mysql-session')(session);
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const { urlencoded } = require('express'); //No sé si es necesario tampoco.
+
 
 app.use(cors());
 app.use(express.json()); // permite que mi app acepte json del lado del cliente
