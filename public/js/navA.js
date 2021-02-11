6e2398dddba0377eee4bc580192268bc8d652a1a
Diego <diego.nicolas.gandara@gmail.com>
2021-02-11 00:08:42 -0300

    Agrego axios y logout.js a todos los archivos .html salvo loginRegistro.html

--- public/js/navA.js
+++ public/js/navA.js
@@ -1,12 +1,12 @@
-const $aNav = document.querySelectorAll(".navbar>a");
-const $aLi = document.querySelectorAll("#nav-li-a>li>a");
+// const $aNav = document.querySelectorAll(".navbar>a");
+// const $aLi = document.querySelectorAll("#nav-li-a>li>a");
 
-console.log($aNav);
-console.log($aLi);
+// console.log($aNav);
+// console.log($aLi);
 
-const $allNavA = [];
-$aNav.forEach(e=> $allNavA.push(e));
-$aLi.forEach(e=> $allNavA.push(e));
-$allNavA.splice(1,1);
-console.log($allNavA);
+// const $allNavA = [];
+// $aNav.forEach(e=> $allNavA.push(e));
+// $aLi.forEach(e=> $allNavA.push(e));
+// $allNavA.splice(1,1);
+// console.log($allNavA);
 
